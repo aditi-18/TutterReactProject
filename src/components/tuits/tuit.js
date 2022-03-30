3cea0fb35c343a4c182356789fcf0a908e6317d9
aditishri1896 <shrivastava.adi@northeastern.edu>
2022-03-30 19:59:36 -0400

    my tuits add

--- src/components/tuits/tuit.js
+++ src/components/tuits/tuit.js
@@ -1,9 +1,69 @@
+// import React from "react";
+// import TuitStats from "./tuit-stats";
+// import TuitImage from "./tuit-image";
+// import TuitVideo from "./tuit-video";
+
+// const Tuit = ({tuit, deleteTuit}) => {
+//   return(
+//     <li className="p-2 ttr-tuit list-group-item d-flex rounded-0">
+//       <div className="pe-2">
+//         {
+//           tuit.postedBy &&
+//           <img src={`../images/${tuit.postedBy.username}.jpg`}
+//                className="ttr-tuit-avatar-logo rounded-circle"/>
+//         }
+//       </div>
+//       <div className="w-100">
+//           <i onClick={() => deleteTuit(tuit._id)} className="fas fa-remove fa-2x fa-pull-right"></i>
+//         <h2
+//           className="fs-5">
+//           {tuit.postedBy && tuit.postedBy.username}
+//           @{tuit.postedBy && tuit.postedBy.username} -
+//           {tuit.published}</h2>
+//         {tuit.tuit}
+//         {
+//           tuit.youtube &&
+//             <TuitVideo tuit={tuit}/>
+//         }
+//         {
+//           tuit.image &&
+//           <TuitImage tuit={tuit}/>
+//         }
+//         <TuitStats tuit={tuit}/>
+//       </div>
+//     </li>
+//   );
+// }
+// export default Tuit;
+
 import React from "react";
 import TuitStats from "./tuit-stats";
 import TuitImage from "./tuit-image";
 import TuitVideo from "./tuit-video";
 
-const Tuit = ({tuit, deleteTuit}) => {
+const Tuit = ({tuit, deleteTuit, likeTuit}) => {
+    const daysOld = (tuit) => {
+        const now = new Date();
+        const nowMillis = now.getTime();
+        const posted = new Date(tuit.postedOn);
+        const postedMillis = posted.getTime();
+        const oldMillis = nowMillis - postedMillis;
+        let old = 0.0;
+        const secondsOld = oldMillis/1000.0;
+        const minutesOld = secondsOld/60.0;
+        const hoursOld = minutesOld/60.0;
+        const daysOld = hoursOld/24.0;
+        if(daysOld > 1) {
+            old = Math.round(daysOld) + 'd';
+        } else if(hoursOld > 1) {
+            old = Math.round(hoursOld) + 'h';
+        } else if(minutesOld > 1) {
+            old = Math.round(minutesOld) + 'm';
+        } else if(secondsOld > 1) {
+            old = Math.round(secondsOld) + 's';
+        }
+        return old;
+    }
   return(
     <li className="p-2 ttr-tuit list-group-item d-flex rounded-0">
       <div className="pe-2">
@@ -19,7 +79,7 @@ const Tuit = ({tuit, deleteTuit}) => {
           className="fs-5">
           {tuit.postedBy && tuit.postedBy.username}
           @{tuit.postedBy && tuit.postedBy.username} -
-          {tuit.published}</h2>
+            <span className="ms-1">{daysOld(tuit)}</span></h2>
         {tuit.tuit}
         {
           tuit.youtube &&
@@ -29,7 +89,7 @@ const Tuit = ({tuit, deleteTuit}) => {
           tuit.image &&
           <TuitImage tuit={tuit}/>
         }
-        <TuitStats tuit={tuit}/>
+        <TuitStats tuit={tuit} likeTuit={likeTuit}/>
       </div>
     </li>
   );
--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const TUITS_API = "https://a2final.herokuapp.com/api/tuits";
-const USERS_API = "https://a2final.herokuapp.com/api/users";
+const TUITS_API = "https://a4final.herokuapp.com/api/tuits";
+const USERS_API = "https://a4final.herokuapp.com/api/users";
 
 
 const api = axios.create({
