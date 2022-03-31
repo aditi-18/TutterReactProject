4a502aed2176880b2c2b883a4c80190e24249344
aditishri1896 <shrivastava.adi@northeastern.edu>
2022-03-31 16:57:01 -0400

    bug fix

--- src/components/profile/login.js
+++ src/components/profile/login.js
@@ -3,7 +3,7 @@ import {useNavigate} from "react-router-dom";
 import {useState} from "react";
 import * as service from "../../services/auth-service";
 
- const Login = () => {
+ export const Login = () => {
     const [loginUser, setLoginUser] = useState({});
     const navigate = useNavigate()
     const login = () =>
@@ -27,5 +27,3 @@ import * as service from "../../services/auth-service";
         </div>
     );
 };
-
-export default Login;
